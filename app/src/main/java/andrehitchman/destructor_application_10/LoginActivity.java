89eeeb1166eea2b6e371734e20d24017986f7242
SandBoxDeveloper <madetobethebest@hotmail.co.uk>
2014-11-21 14:11:00 +1000

    Major UI Changes
    - App Launcher icon added.
    - Action bar hidden on sign-in activity.
    - Title of app with subtitle added to Login activity screen.
    - Login activity layout UI change
            - Custom theme
            - Custom button

--- app/src/main/java/andrehitchman/destructor_application_10/LoginActivity.java
+++ app/src/main/java/andrehitchman/destructor_application_10/LoginActivity.java
@@ -1,5 +1,6 @@
 package andrehitchman.destructor_application_10;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -29,6 +30,10 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS); // needs to be set before setContentView
         setContentView(R.layout.activity_login);
+
+        ActionBar actionBar = getActionBar();
+        // hide actionbar
+        actionBar.hide();
 
         mSignUpTextView = (TextView)findViewById(R.id.signUpText);
         // create onClickListener for signUp
